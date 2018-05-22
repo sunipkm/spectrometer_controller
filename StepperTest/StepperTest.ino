3dfb6508c48ecd4f819c93268964ac144d851700
Sunip Mukherjee <sunipkmukherjee@gmail.com>
2018-05-22 10:53:58 -0700

    IRQ handlers for both limit switchess

--- StepperTest/StepperTest.ino
+++ StepperTest/StepperTest.ino
@@ -3,11 +3,13 @@
 
 #define STEPMOT 1
 int irPin1 = 2 ;
+int irPin2 = 3 ;
 volatile byte state = FORWARD ;
 volatile int stepMot = STEPMOT ;
 volatile int motStop = 0 ;
 int dispCount = 0 ;
-volatile byte limitsw = 1 ;
+volatile byte limitsw1 = 1 ;
+volatile byte limitsw2 = 1 ;
 // Create the motor shield object with the default I2C address
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 // Or, create it with a different I2C address (say for stacking)
@@ -31,7 +33,8 @@ void setup() {
 }
 
 void loop() {
-  check_irq();
+  check_irq1();
+  check_irq2();
   myMotor->step(stepMot, state, DOUBLE);
   if ( stepMot > 0 ) {
     dispCount = dispCount + 1 ;
@@ -39,29 +42,43 @@ void loop() {
   check_serial();
 }
 
-void check_irq()
+void check_irq1() //limitswitch 1
 {
-    if ( digitalRead(irPin1) == HIGH && limitsw )
+    if ( digitalRead(irPin1) == HIGH && limitsw1 ) //if the pin is set AND it has been hit the first time (to get rid of the multiple change direction problem)
   { 
-    Serial.println("Hit limit switch");
-    limitsw = 0 ;
-    chdir() ;
-    myMotor->step(stepMot, state, DOUBLE);
+    Serial.println("Hit limit switch"); //let us know
+    limitsw1 = 0 ; //so that doesn't enter the function even if the switch is high for quite a few loops
+    chdir() ; //reverse, but in the real case should just change direction of motion
+    myMotor->step(stepMot, state, DOUBLE); //step back a bit
     if ( stepMot > 0 )
-     dispCount = dispCount - 1 ;
-    stepMot = 0 ;
-    motStop = 1 ;
+     dispCount = dispCount - 1 ; //displaced by one step backward the first time
+    stepMot = 0 ; //stop motor
+    motStop = 1 ; //indicate that motor has stopped
+  }
+}
+void check_irq2() //limitswitch 1
+{
+    if ( digitalRead(irPin2) == HIGH && limitsw2 ) //if the pin is set AND it has been hit the first time (to get rid of the multiple change direction problem)
+  { 
+    Serial.println("Hit limit switch"); //let us know
+    limitsw1 = 0 ; //so that doesn't enter the function even if the switch is high for quite a few loops
+    chdir() ; //reverse, but in the real case should just change direction of motion
+    myMotor->step(stepMot, state, DOUBLE); //step back a bit
+    if ( stepMot > 0 )
+     dispCount = dispCount - 1 ; //displaced by one step backward the first time
+    stepMot = 0 ; //stop motor
+    motStop = 1 ; //indicate that motor has stopped
   }
 }
 
-void check_serial()
+void check_serial() //check serial for input
 {
-  if ( motStop )
+  if ( motStop ) //only when motor is stopped
   {
-    if (Serial.available() > 0)
+    if (Serial.available() > 0) //if only serial input is available
     {
-      char inCh = Serial.read() ;
-      if ( inCh == 'h' )
+      char inCh = Serial.read() ; //read char from serial
+      if ( inCh == 'h' ) //go home
       {
         stepMot = STEPMOT ;
         Serial.println("Going home...") ;
@@ -72,26 +89,28 @@ void check_serial()
         stepMot = 0 ;
         motStop = 1 ;
       }
-      if ( inCh == 'd' ) {
-      Serial.println("Changing state...");
-      Serial.print("Initial state: ") ;
-      Serial.println(state);
+      if ( inCh == 'd' ) //change direction 
+      {
+        //Serial.println("Changing state...");
+        Serial.print("Initial state: ") ;
+        Serial.println(state);
         chdir() ;
-      Serial.print("Final state: ") ;
-      Serial.println(state);
+        Serial.print("Final state: ") ;
+        Serial.println(state);
       }
-      if ( inCh == 's' )
+      if ( inCh == 's' ) //restart motor
       {
         Serial.println("Restarting motor.");
         stepMot = STEPMOT ;
         motStop = 0 ;
-        limitsw = 1 ;
+        limitsw1 = 1 ;
       }
     }
   }
 }
 
-void chdir(){
+void chdir()
+{
   Serial.println("Changing dir...");
   if ( state == FORWARD )
     state = BACKWARD ;
