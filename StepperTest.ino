61f6b77eb6349265f72cffdb9009093788a0e0ec
Sunip Mukherjee <sunipkmukherjee@gmail.com>
2018-05-22 09:34:51 -0700

    Serial Console Restart Only

--- StepperTest.ino
+++ StepperTest.ino
@@ -1,18 +1,10 @@
-/* 
-This is a test sketch for the Adafruit assembled Motor Shield for Arduino v2
-It won't work with v1.x motor shields! Only for the v2's with built in PWM
-control
-
-For use with the Adafruit Motor Shield v2 
----->	http://www.adafruit.com/products/1438
-*/
-
-
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
+
 int irPin1 = 2 ;
 volatile byte state = FORWARD ;
 volatile int stepMot = 20 ;
+volatile int motStop = 0 ;
 
 // Create the motor shield object with the default I2C address
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
@@ -26,13 +18,14 @@ Adafruit_StepperMotor *myMotor = AFMS.getStepper(200, 2);
 
 void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
-  Serial.println("Stepper test!");
+  Serial.println("System up!");
 
   AFMS.begin();  // create with the default frequency 1.6KHz
   //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
   
   myMotor->setSpeed(20);  // 10 rpm 
-  pinMode(irPin1, INPUT) ;
+  pinMode(irPin1, INPUT) ; //setting pin irPin1 for limit switch 1
+  //keyboard.begin();
   //attachInterrupt(digitalPinToInterrupt(irPin), mot_stop, HIGH);
   //int i = 0 ;  
 }
@@ -46,8 +39,18 @@ void loop() {
       state = FORWARD ;
     myMotor->step(stepMot, state, DOUBLE);
     stepMot = 0 ;
+    motStop = 1 ;
   }
-  myMotor->step(stepMot, state, DOUBLE); 
+  myMotor->step(stepMot, state, DOUBLE);
+  if ( motStop )
+  {
+    if (Serial.available() > 0)
+    {
+      char inCh = Serial.read() ;
+      if ( inCh == 's' )
+        stepMot = 20 ;
+    }
+  }
   //myMotor->step(0, BACKWARD, DOUBLE);
 //  
 //  Serial.println("Interleave coil steps");
